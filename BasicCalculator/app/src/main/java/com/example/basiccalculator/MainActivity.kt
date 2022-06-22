27db0b0ccef753370f1a938eb19984303bac74e0
Phan Tài Đức <phantaiduc2005@gmail.com>
2022-06-22 14:26:31 +0700

    Create 10 func

--- BasicCalculator/app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ BasicCalculator/app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.basiccalculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.basiccalculator.databinding.ActivityMainBinding
@@ -11,6 +12,94 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+//        binding.plusBtn.setOnClickListener { calFunc() }
+//        binding.minusBtn.setOnClickListener { calFunc() }
+//        binding.multiplicationBtn.setOnClickListener { calFunc() }
+//        binding.divisionBtn.setOnClickListener { calFunc() }
+//        binding.openParenthesesBtn.setOnClickListener { calFunc() }
+//        binding.closeParenthesesBtn.setOnClickListener { calFunc() }
+//        binding.calculateBtn.setOnClickListener { calFunc() }
+        binding.clearBtn?.setOnClickListener { clearFunc() }
+        binding.deleteBtn.setOnClickListener { clearFunc() }
+        binding.numOneBtn?.setOnClickListener { numOneFunc() }
+        binding.numTwoBtn?.setOnClickListener { numTwoFunc() }
+        binding.numThreeBtn?.setOnClickListener { numThreeFunc() }
+        binding.numFourBtn?.setOnClickListener { numFourFunc() }
+        binding.numFiveBtn?.setOnClickListener { numFiveFunc() }
+        binding.numSixBtn?.setOnClickListener { numSixFunc() }
+        binding.numSevenBtn?.setOnClickListener { numSevenFunc() }
+        binding.numEightBtn?.setOnClickListener { numEight() }
+        binding.numNineBtn?.setOnClickListener { numNineFunc() }
+        binding.numZeroBtn?.setOnClickListener { numZeroFunc() }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numZeroFunc() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "0")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numNineFunc() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "9")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numEight() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "8")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numSevenFunc() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "7")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numSixFunc() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "6")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numFiveFunc() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "5")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numFourFunc() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "4")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numThreeFunc() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "3")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numTwoFunc() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "2")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun numOneFunc() {
+        val resultField = binding.resultValue.text.toString()
+        binding.resultValue.setText(resultField + "1")
+    }
+
+    private fun clearFunc() {
+        binding.resultValue.setText("")
+    }
+
+    private fun calFunc() {
+        TODO("Not yet implemented")
     }
 }
